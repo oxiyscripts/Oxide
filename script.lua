bb7f03d00fa508588a09b580284fb83a7b7cc0d5
oxiyscripts <115106014+oxiyscripts@users.noreply.github.com>
2023-02-14 11:57:53 +0000

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,16 +1,1 @@
-local url = "https://raw.githubusercontent.com/ccenta/Oxide/main/scripts/"
-
-local games = {
-  [537413528] = "Build a boat";
-  [5602055394] = "Hood Modded";
-  [192800] = "Pizza Place";
-  [2788229376] = "Da Hood Autoarrest";
- 
-}
-
-for i,v in next, games do
-    games[i] = table.concat(v:split(' '), '_')
-end
-
-local name = games[game.PlaceId] or games[game.GameId]
-return loadstring(game:HttpGet(url.. "/"..(name or "Universal")..".lua"))()
+local a="https://raw.githubusercontent.com/ccenta/Oxide/main/scripts/"local b={[537413528]="Build a boat",[5602055394]="Hood Modded",[192800]="Pizza Place",[2788229376]="Da Hood Autoarrest"}for c,d in next,b do b[c]=table.concat(d:split(' '),'_')end;local e=b[game.PlaceId]or b[game.GameId]return loadstring(game:HttpGet(a.."/"..(e or"Universal")..".lua"))()
